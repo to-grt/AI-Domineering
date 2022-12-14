e4d4be62b86be030330b19392fac83dcc4bd2ab2
Théo <tgueuret@live.fr>
2022-12-14 16:25:32 +0100

    add IARand

---
+++ IARand.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class IARand:
+
+    def __init__(self) -> None:
+        pass
+        
+    def __repr__(self) -> str:
+        return "I am an AI that choose a random move to play"
+
+    # choose a random move in the moves available and returns it
+    def choose_move(self, board):
+        index_move = np.random.randint(0,board[-1]-1)
+        return board[index_move]
